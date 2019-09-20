5d65a2bc9b8b8d0b1282202fb0497d8a95637ed0
Haixin <46263698+HaixinLiuNeuro@users.noreply.github.com>
2019-09-20 16:46:21 -0400

    add in

---
+++ plot_jbfill_mean_se.m
@@ -0,0 +1,13 @@
+function plot_jbfill_mean_se (mtx, x_point,colorstr)
+%function [figh] = plot_jbfill_mean_se (mtx,x_point,colorstr)
+%this function use jbfill function to plot mean and se for input matrix
+%%
+
+% figh = gcf;%
+% figh = figure;hold on;
+% pbaspect([1.66 1 1])
+mtx_mean = nanmean(mtx,1);
+mtx_se = sqrt(nanvar(mtx,1,1)./size(mtx,1)); %?? 1 -- use N
+plot(x_point,mtx_mean,'color',colorstr);
+jbfill(x_point,mtx_mean+mtx_se,mtx_mean-mtx_se,colorstr,colorstr,1,0.3); hold on;
+end
